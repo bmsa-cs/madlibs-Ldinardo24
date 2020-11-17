35aae349e07565b0beca1970297bc54f99e5d154
Lourden DiNardo <ldinardo24@biomedscienceacademy.org>
2020-11-17 16:11:06 +0000

    I added my name/core

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Lourden DiNardo 
+Period/Core: Core 6 
 
 
 """
+
+print("Enter a (a persons name)")
+#Mia
+madlib1 = str(input())
+print("Enter an adjective (a descriptive word)")
+#big
+madlib2 = str(input())
+print("Enter an adjective (descriptive word)")
+#Tiny
+madlib3 = str(input())
+print("Enter a noun (person,place, or thing)")
+#Kid
+madlib4 = str(input())
+print("Enter an adjective (descriptive word)")
+#Yellow
+madlib5 = str(input())
+print("Enter a noun (person,place,or thing)")
+#Cart
+madlib6 = str(input())
+print("Enter adjective (descriptive word)")
+#big
+madlib7 = str(input())
+print("Enter an adjective (descriptive word)")
+#green 
+madlib8 = str(input())
+print("Enter a verb (action word)")
+#Rain 
+madlib9 = str(input())
+print("Enter an adjective (descriptive word)")
+#Wet
+madlib10 = str(input())
+
+print(" Yesterday, " + madlib1 + " and I went to the park. On our way to the " + madlib2 + " park, we saw a " + madlib3 + " on a bike. We also saw big " + madlib4 + " balloons tied to a " + madlib5 + " Once we got to the " + madlib6 + " park, the sky turned " + madlib7 + " It started  to " + madlib8 + " So we decided to go home because we didn't want to get " +madlib9 + " The End ")
+
